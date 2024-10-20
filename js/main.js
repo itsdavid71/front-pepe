5d4046560e2aeafa3e09832be68f0f6b3a83ba59
David <itsdavid71@gmail.com>
2024-10-21 02:27:32 +0300

    Update

--- js/main.js
+++ js/main.js
@@ -35,6 +35,32 @@ class Wall extends Cell {
         return false
     }
 }
+
+class MegaWall extends Cell {
+    type = "megawall"
+    constructor(x, y) {
+        super(x, y)
+        this.element.classList.add(this.type)
+    }
+
+    canMove() {
+        return false
+    }
+}
+
+class AngryWall extends Cell {
+    type = "angrywall"
+    damage = 1
+    constructor(x, y) {
+        super(x, y)
+        this.element.classList.add(this.type)
+    }
+
+    canMove() {
+        return false
+    }
+}
+
 class Money extends Cell {
     type = "money"
     constructor(x, y) {
@@ -59,8 +85,9 @@ class Slow extends Cell {
 
 class Robot {
     position = new Point(5, 5) // [x, y] // new Point(5, 5)
+    health = 3
     element = document.createElement("div")
-    constructor() { // point // x, y
+    constructor(type) { // point // x, y
         this.element.classList.add("robot")
     }
 
@@ -91,22 +118,36 @@ class Robot {
     }
 }
 
+class EnemyRobot extends Robot {
+    damage = 3
+    constructor(x, y) { // point // x, y
+        super ()
+        this.position = new Point(x, y)    
+        this.element.classList.add("enemy")
+        
+    }
+}
+
+
 class Game {
     field = []
     robot = new Robot()
+    enemies = []
     score = 0
     coinsCount = 0
     isSlow = false
     level = 1
+    popup = new Popup()
 
-    coinSound = new Audio('audio/coin.wav');
-    coinDropSound = new Audio('audio/coinDrop.mp3');
-    hitSound = new Audio('audio/hit.mp3');
-    levelUpSound = new Audio('audio/victory.mp3');
-    boostSound = new Audio('audio/boost.mp3');
-    webSound = new Audio('audio/web.mp3');
+    coinSound = new Audio('audio/coin.wav')
+    coinDropSound = new Audio('audio/coinDrop.mp3')
+    hitSound = new Audio('audio/hit.mp3')
+    levelUpSound = new Audio('audio/victory.mp3')
+    boostSound = new Audio('audio/boost.mp3')
+    webSound = new Audio('audio/web.mp3')
     victorySound = new Audio('audio/victory.mp3')
     tavernSound = new Audio('audio/tavern.mp3')
+    eneymDeathSound = new Audio('audio/enemy-death.wav')
 
     constructor(root) {
         this.root = root // document.getElementById("app")
@@ -140,6 +181,16 @@ class Game {
                     case random % 10 === 7:
                         cell = new Slow(x, y);
                         break;
+                    case random % 10 === 8:
+                        cell = new MegaWall(x, y);
+                        break;
+                    case random % 10 === 9:
+                        cell = new AngryWall(x, y);
+                        break;
+
+                    // case random % 10 === 5:
+                    //     cell = new EnemyRobot(x, y);
+                    //     break;
                     default:
                         cell = new Cell(x, y)
                 }
@@ -164,11 +215,16 @@ class Game {
     }
 
     renderRobot() {
+        const healthBar = document.createElement('p')
+        healthBar.innerHTML = 'Здоровье: ' + this.robot.health
+        healthBar.classList.add('health-bar')
+        this.fieldElement.append(healthBar)
         this.field[this.robot.position.y][this.robot.position.x]
             .element
             .appendChild(this.robot.element)
 
     }
+    
 
     changeDirection(robot, direction) {
         robot.classList.remove('robot-top', 'robot-left', 'robot-right', 'robot-bottom', 'robot-undefined')
@@ -190,9 +246,19 @@ class Game {
         this.level++
         this.score = 0
         this.fieldElement.classList.add(`background-${this.level}`)
+        this.popup.hide()
+    }
 
-        document.getElementById("popup").style.display = 'none'
-        document.getElementById("popup-content").innerHTML = ''
+
+    restartGame() {
+        this.field = []
+        this.createField()
+        this.renderField()
+        this.robot.position = new Point(5, 5)
+        this.render()
+        this.score = 0
+        this.robot.health = 3
+        this.popup.hide()
     }
 
     checkNextCell(field, robot, direction) {
@@ -257,18 +323,33 @@ class Game {
                     case "KeyD":
                         direction = "right";
                         break;
-                }
+                }        
+
                 this.changeDirection(this.robot.element, direction)
                 if (direction) { // direction !== undefined
                     let nextCell = this.checkNextCell(this.field, this.robot, direction)
+                    
 
                     this.hitSound.pause()
                     if (nextCell !== undefined) {
-                        if (!nextCell.canMove() && nextCell.health > 1) {
+                        if (!nextCell.canMove() &&  nextCell.health > 1) {
                             nextCell.health--
                             nextCell.element.classList.add('wall-' + nextCell.health)
                             this.hitSound.play()
                             this.hitAnimation(this.robot.element, direction)
+                        } else if (!nextCell.canMove() && nextCell.type === 'angrywall') {
+                            // nextCell.health--
+                            this.robot.health--
+                            if (this.robot.health === 0) {
+                                this.popup.text = `Вы проиграли =(`
+                                        // this.popup.action = () => startNewGame()
+                                this.popup.action = () => this.restartGame() 
+                                this.popup.button = 'Попробовать еще раз'
+                                this.popup.show()
+                            }
+                            // nextCell.element.classList.add('wall-' + nextCell.health)
+                            // this.hitSound.play()
+                            // this.hitAnimation(this.robot.element, direction)
                         } else if (!nextCell.canMove() && nextCell.health == 1) {
                             this.field[nextCell.position.y][nextCell.position.x] = new Money()
                             this.coinsCount++
@@ -276,7 +357,6 @@ class Game {
 
                         } else if (!this.isSlow && nextCell && nextCell.canMove()) { // type !== wall
                             this.robot.walk(direction)
-
                             const currentCell = this.field[this.robot.position.y][this.robot.position.x]
                             switch (currentCell.type) {
                                 case "money":
@@ -284,20 +364,19 @@ class Game {
                                     this.field[this.robot.position.y][this.robot.position.x] = new Cell()
                                     this.coinsCount--
                                     this.coinSound.play()
-
                                     // прошли игру
                                     if (this.coinsCount === 0) {
-                                        const popup = new Popup(`Вы прошли`, this.score, this.level)
-                                        this.victorySound.play()
-                                        const nextLevelButton = document.getElementById('next-level-button')
-                                        nextLevelButton.addEventListener('click', event => this.startNewGame(popup))
-                                        popup.show
+                                        this.popup.text = `Вы прошли уровень ${this.level} и набрали ${this.score}`
+                                        this.popup.action = () => this.startNewGame() 
+                                        this.popup.button = 'Следующий уровень'
+                                        this.popup.show()
+                                        const popupElement = this.popup.contentElement
                                     }
                                     break;
                                 case "force":
                                     let nextCell = this.checkNextCell(this.field, this.robot, direction)
                                     if (nextCell) {
-                                        if (nextCell.type !== 'wall' && nextCell) {
+                                        if (nextCell.type !== 'wall' && nextCell.type !== 'megawall' && nextCell.type !== 'angrywall' && nextCell) {
                                             this.robot.walk(direction)
                                             this.boostSound.play()
                                             if (this.field[this.robot.position.y][this.robot.position.x].type == "money") {
@@ -310,7 +389,7 @@ class Game {
                                                     if (this.field[this.robot.position.y][this.robot.position.x].type == "force") {
                                                         let nextCell = this.checkNextCell(this.field, this.robot, direction)
                                                         if (nextCell) {
-                                                            if (nextCell.type !== 'wall') {
+                                                            if (nextCell.type !== 'wall' && nextCell.type !== 'megawall' && nextCell.type !== 'angrywall') {
                                                                 this.robot.walk(direction)
                                                                 if (this.field[this.robot.position.y][this.robot.position.x].type == "money") {
                                                                     this.score++
@@ -318,7 +397,6 @@ class Game {
                                                                     this.coinsCount--
                                                                 }
                                                                 if (this.field[this.robot.position.y][this.robot.position.x].type == 'wood') {
-                                                                    console.log('ДУБИНА')
                                                                 }
                                                             }
                                                         }
@@ -335,7 +413,20 @@ class Game {
                         }
                     }
 
+
+                    // if (this.field[this.robot.position.y][this.robot.position.x] === this.field[this.enemies.position.y][this.enemies.position.x]) {
+                    //     this.robot.health = 0
+                    //     if (this.robot.health === 0) {
+                    //         this.popup.text = `Вас убили!`
+                    //                 // this.popup.action = () => startNewGame()
+                    //         this.popup.action = () => this.restartGame() 
+                    //         this.popup.button = 'Попробовать еще раз'
+                    //         this.popup.show()
+                    //     }
+                    // }
+
                     this.render()
+                    
                 }
             }
         })
@@ -349,21 +440,101 @@ class Game {
         this.fieldElement.innerText = ""
         this.renderField()
         this.renderRobot()
+        this.renderEnemiesRobot()
     }
+
+    enemiesStep() {
+        let directions = ['top', 'left', 'bottom', 'right']
+            for (const enemy of this.enemies) {
+                let randomDirection = directions[Math.floor(Math.random() * 4)]
+                const index = this.enemies.indexOf(enemy)
+                let nextEnemyCell = this.checkNextCell(this.field, enemy, randomDirection)
+                // управляем врагом
+                if (nextEnemyCell !== undefined) {
+                    if (!nextEnemyCell.canMove() &&  nextEnemyCell.health > 1) {
+                        nextEnemyCell.health--
+                        nextEnemyCell.element.classList.add('wall-' + nextEnemyCell.health)
+                    } else if (!nextEnemyCell.canMove() && nextEnemyCell.type === 'angrywall') {
+                        enemy.health--
+                        if (enemy.health === 0) {
+                            this.enemies.splice(index, 1)
+                            this.eneymDeathSound.play()
+                            enemy.element.remove()
+                        }
+                    } else if (!nextEnemyCell.canMove() && nextEnemyCell.health == 1) {
+                        this.field[nextEnemyCell.position.y][nextEnemyCell.position.x] = new Money()
+                        this.coinsCount++
+                        this.coinDropSound.play()
+    
+                    } else if ( nextEnemyCell && nextEnemyCell.canMove()) { // type !== wall
+                        enemy.walk(randomDirection)
+                    }
+                    if (this.field[this.robot.position.y][this.robot.position.x] === this.field[enemy.position.y][enemy.position.x]) {
+                        this.robot.health = 0
+                        if (this.robot.health === 0) {
+                            this.popup.text = `Вас убили!`
+                                    // this.popup.action = () => startNewGame()
+                            this.popup.action = () => this.restartGame() 
+                            this.popup.button = 'Попробовать еще раз'
+                            this.popup.show()
+                        }
+                    }
+                }
+            }
+            this.render()
+    }
+
+    createEnemies() {
+        this.enemies.push(
+            new EnemyRobot(1, 1),
+            new EnemyRobot(4, 9),
+            new EnemyRobot(6, 7),
+        )
+    }
+    
+    renderEnemiesRobot() {
+        for(const enemy of this.enemies) {
+            this.field[enemy.position.y][enemy.position.x]
+                .element
+                .appendChild(enemy.element)
+        }
+    }
+
+    
+
 }
 
 class Popup {
     content = document.getElementById('popup-content')
     popup = document.getElementById('popup')
+    buttonElement = document.getElementById('popup-button')
 
-    constructor(text, score, level, newGame) {
-        console.log(newGame)
-        this.content.insertAdjacentHTML('afterbegin',
-            `<h2>${text} уровень ${level}</h2>
-            <p>Количество собранных монеток: ${score}
-            <button id="next-level-button">Следующий уровень</button>
-            `)
-        this.show()
+
+    // get contentElement() {
+    //     this.content.innerText += 'some Text'
+    //     return this.content
+    // }
+    set text(value) {
+        this.content.innerHTML = value
+    }
+    set action(value) {
+        this.buttonElement.onclick = value
+    }
+    set button(value) {
+        this.buttonElement.innerText = value;
+    }
+    // constructor(text, score, level, newGame, action) {
+    //     this.content.insertAdjacentHTML('afterbegin',
+    //         `<h2>${text} уровень ${level}</h2>
+    //         <p>Количество собранных монеток: ${score}
+    //         <button id="next-level-button">Следующий уровень</button>
+    //         `)
+    //     this.show()
+    // }
+    constructor() {
+        // this.content.innerHTML = text
+        // this.button.oncancel = action
+        this.hide()
     }
 
     hide() {
@@ -378,3 +549,13 @@ const game = new Game(document.getElementById("app"))
 game.createField()
 game.renderField()
 game.renderRobot()
+game.createEnemies()
+game.renderEnemiesRobot()
+
+const enemiesStart = setInterval(() => {
+    game.enemiesStep()
+    if (game.enemies == []) {
+        alert(123)
+        clearInterval(enemiesStart)
+    }
+}, 400);
